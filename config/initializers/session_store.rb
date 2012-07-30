93f37f2bcbc1e42db66c11a7c4fecd1ddc37640b
Alex Smolen <alexsmolen@twitter.com>
2012-07-30 15:03:23 -0700

    Changed hash format to support Ruby < 1.9. Added author byline

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Nopassword::Application.config.session_store :cookie_store, key: '_nopassword_session', :expire_after => 10.years
+Nopassword::Application.config.session_store :cookie_store, :key => '_nopassword_session', :expire_after => 10.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
