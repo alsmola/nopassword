9faf43a2b2f8248ef40af737104ead24039c0f5d
Alex Smolen <alexsmolen@twitter.com>
2012-08-10 16:21:41 -0700

    Updated email to show friendly browser name, better formatting

--- app/controllers/nopassword_controller.rb
+++ app/controllers/nopassword_controller.rb
@@ -1,6 +1,6 @@
 class NopasswordController < ApplicationController
 
-  EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
 
   def send_login_email
     email = request[:email]
--- app/mailers/no_password_emails.rb
+++ app/mailers/no_password_emails.rb
@@ -1,12 +1,14 @@
 class NoPasswordEmails < ActionMailer::Base
+  include ApplicationHelper
+
   default :from => 'nopassword@alexsmolen.com',
     :return_path => 'nopassword@alexsmolen.com'
 
   def no_password_email(email, id, time, remote_ip, user_agent, code, host)
     @id = id
-    @time = time.strftime("%e %b %Y %H:%m%p")
+    @time = time.strftime("%e %b %Y %H:%m")
     @remote_ip = remote_ip
-    @user_agent = user_agent
+    @user_agent = browser_name(user_agent)
     @code = code
     @email = email
     @host = host
