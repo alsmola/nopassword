12067838ac368655090fe361b83d8a9868909f67
Alex Smolen <me@alexsmolen.com>
2013-06-30 09:35:21 -0700

    Resolved merge

--- app/helpers/nopassword/application_helper.rb
+++ app/helpers/nopassword/application_helper.rb
@@ -3,9 +3,5 @@ module Nopassword
     def friendly_time(time)
       time_ago_in_words(time) + " ago"
     end
-
-    def browser_name(ua)
-      Browser.new(:ua => ua).to_s.split(' ')[0].capitalize
-    end
   end
 end
