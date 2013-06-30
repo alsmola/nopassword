77e97d8cf557bf445db7ea250dd09dd84a3d3cda
Alex Smolen <me@alexsmolen.com>
2013-06-30 11:45:11 -0700

    Removed secret token

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,10 @@
-# Be sure to restart your server when you modify this file.
+ENV['passw3rd-cipher_name'] = 'aes-256-cbc'
+if %w{production staging}.include? ENV['RAILS_ENV']
+  ENV['passw3rd-password_file_dir'] = Rails.root.join('config', 'passwords', 'production').to_s
+  ENV['passw3rd-key_file_dir'] = Rails.root.join('config', 'passwords', 'production').to_s
+else
+  ENV['passw3rd-password_file_dir'] = Rails.root.join('config', 'passwords').to_s
+  ENV['passw3rd-key_file_dir'] = Rails.root.join('config', 'passwords').to_s
+end
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '4741122d64e015eea36b648d7d435d7b75f3ee2b35215b76a39eca42bc4fcbfd295d92d5b020f05e2f859bf5898fefa23eb4ee08f83e1821c44424a7fcba1a62'
+Dummy::Application.config.secret_token = Passw3rd::PasswordService.get_password('secret_token')
